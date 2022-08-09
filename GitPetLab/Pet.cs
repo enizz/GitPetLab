e7ac90b4a6796cd7a29837c47191bdf9844dc637
Eugene <Eugene.Nisnevich@Gmail.com>
2022-08-08 21:31:21 -0400

    added pet class

---
+++ GitPetLab/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPetLab
+{
+    internal abstract class Pet
+    {
+        public string Name { get; set; }
+        public virtual void MakeHappyNoise()
+        {
+            Console.WriteLine("caw caw!");
+        }
+    }
+}
